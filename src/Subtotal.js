beb77eacc31e9602671567a8dbe2c46fb2b0bc42
NPVallejos <nvallejos123@gmail.com>
2020-12-19 17:30:20 -0500

    Added Checkout Component and Subtotal Component
    Render Checkout component at './checkout' route

---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Subtotal.css';
+
+function Subtotal() {
+    return (
+        <div>
+            <h1>Subtotal Component Goes Here!</h1>
+        </div>
+    )
+}
+
+export default Subtotal;
