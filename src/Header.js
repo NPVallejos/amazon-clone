3d2051c4e3800a6160e53278efac05fdd09e7aff
NPVallejos <nvallejos123@gmail.com>
2020-12-18 19:36:07 -0500

    Installed @material-ui and @material-ui/icon
    Edited Header.css:
    1. search bar now spans entire div
    2. search bar now has search icon taken from material-ui/icon/Search
    3. all header items are now responsive (flex items)

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 /*
 ** This component is the navbar for the website
@@ -18,7 +19,7 @@ function Header() {
             
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
-                {/* Logo */}
+                <SearchIcon className="header__searchIcon" />
             </div>
 
             <div className="header__nav">
