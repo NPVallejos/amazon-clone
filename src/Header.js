16dc588de3de0d0c7aa7716677dc631aaf0e1c9c
NPVallejos <nvallejos123@gmail.com>
2020-12-19 17:19:31 -0500

    Adding link to logo in Header.js
    Redirects to homepage '/'

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 // material-ui imports
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 /*
 ** This component is the navbar for the website
@@ -15,10 +16,12 @@ import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
     return (
         <div className='header'>
-            <img 
-                className="header__logo" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />
+            <Link to='/'>
+                <img 
+                    className="header__logo" 
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
             
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
