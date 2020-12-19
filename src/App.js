7258367d9d356a37ff4a3240a57f7c151ccd26ec
NPVallejos <nvallejos123@gmail.com>
2020-12-19 16:59:45 -0500

    Installed react-router-com module
    Implemented two basic routes
    +'/' => default route
    +'/checkout' => checkout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Header from './Header.js';
 import Home from './Home.js';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Welcome to the checkout page!</h1>
+          </Route>
+          
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
