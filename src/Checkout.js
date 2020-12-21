6841faa5e29582d82ea744ee9f8ddef0cd07103d
NPVallejos <nvallejos123@gmail.com>
2020-12-20 19:53:17 -0500

    Render items from basket on checkout page
    +CheckoutProduct.js -> represent the product to be displayed on the
    checkout page
    +CheckoutProduct.css
    +README.md -> updated readme with important tools used so far in the
    project

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal.js';
+import { useStateValue } from './StateProvider.js';
+import CheckoutProduct from './CheckoutProduct.js';
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -16,7 +20,17 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping basket
                     </h2>
-                    {/* BasketItem */}
+
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />                        
+                    ))}
+
                 </div>
             </div>
 
