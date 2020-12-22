d1632dcaba6accf23d162dd060bee61898729e3c
NPVallejos <nvallejos123@gmail.com>
2020-12-22 18:35:22 -0500

    Created Login page
    New:
    +Login.js -> Login page component
    +Login.css
    +installed firebase node module
    
    Modifed:
    -App.js -> Route path to '/login' setup to display Login component
    -Header.js -> Now contains a link to '/login' page when clicking on
    'Hello Guest Sign In'

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import './App.css';
 import Header from './Header.js';
 import Home from './Home.js';
 import Checkout from './Checkout.js';
+import Login from './Login.js';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-        
         <Switch>
-          
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         
